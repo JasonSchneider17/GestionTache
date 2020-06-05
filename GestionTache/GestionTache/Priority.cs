e026f78059bc8d6d569f9c18458fc29050bb0d4c
JasonSchneider17 <45227080+JasonSchneider17@users.noreply.github.com>
2020-06-05 11:43:28 +0200

    Ajout des classe task et priority

---
+++ GestionTache/GestionTache/Priority.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTache
+{
+    public class Priority
+    {
+        private string name;
+        private int degreePriority;
+
+
+        public Priority()
+        {
+
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+
+        public int DegreePriority
+        {
+            get { return this.degreePriority; }
+            set { this.degreePriority = value; }
+        }
+    }
+}
---
+++ GestionTache/GestionTache/Task.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTache
+{
+   public class Task
+    {
+        private string name;
+        private string comment;
+        private Priority priority;
+
+
+        public Task()
+        {
+
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string Comment
+        {
+            get { return this.comment; }
+            set { this.comment = value; }
+        }
+
+        public Priority Priority
+        {
+            get { return this.priority; }
+            set { this.priority = value; }
+        }
+
+
+    }
+}
